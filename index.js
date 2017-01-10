79346c36e17e3b5da7fd4b355a8a189d591470d4
dvquangt <dvquang.it@gmail.com>
2017-01-10 16:24:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,5 +17,33 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+app.get('/db', function (request, response) {
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+	  if (err) throw err;
+	  console.log('Get data from server...');
+	  client.query('SELECT hohocsinh__c, tenhocsinh__c FROM salesforce.hocsinh__c;', function(err, result) {
+		 if (err){
+			console.error(err); response.send("Error " + err); 
+		}else{ 
+			response.render('pages/db', {results: result.rows} ); 
+			}
+		});
+	});
+});
 
+app.get('/db2', function (request, response) {
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+	  if (err) throw err;
+	  console.log('Get data from server...');
+	  client.query('SELECT name FROM salesforce.account;', function(err, result) {
+		 if (err){
+			console.error(err); response.send("Error " + err); 
+		}else{ 
+			response.render('pages/db2', {results: result.rows} ); 
+			}
+		});
+	});
+});
 
