d5807acf48cc5b22854350b2ddd21d243898dde5
dvquangt <dvquang.it@gmail.com>
2017-01-12 11:20:10 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/getOrder', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
 	  	if (err) throw err;
 	  	console.log('Get data from server...');
-	  	client.query('SELECT Order__c, CustomerName__c, ProductName__c FROM Order__c ;', function(err, result) {
+	  	client.query('SELECT Order__c, CustomerName__c, ProductName__c FROM salesforce.Order__c ;', function(err, result) {
 		 	if (err){
 				console.error(err); response.send("Error " + err); 
 			}else{ 
