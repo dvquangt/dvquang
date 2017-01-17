6ba265483f41d7dedc7eb81a96063e4dc9e77459
dvquangt <dvquang.it@gmail.com>
2017-01-17 14:34:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,18 +9,35 @@ var connection = pg.connect(process.env.DATABASE_URL, function(err, client) {
 		connection = client;
 	});
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
+});
+
+
 // Start SocketIO
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-server.listen(8080);
-// var socketIO = require('socket.io'),
-//     http = require('http'),
-//     server = http.createServer().listen(8080, function(){
-//         console.log('Started Socket.IO');
-//     }),
-//     io = socketIO.listen(server);
-// io.set('match origin protocol', true);
-// io.set('origins', '*:*');
+server.listen(app.get('port'));
 var run = function (socket){
     socket.on('user-join', function (data) {
         console.log('User ' + data + ' have joined');
@@ -36,29 +53,6 @@ var run = function (socket){
 io.on('connection', run);
 // end SocketIO
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    next();
-});
 
 app.get('/api/getOrder', function (request, response) {
   	connection.query('SELECT id, Order__c, CustomerName__c, ProductName__c FROM salesforce.Order__c ;', function(err, result) {
